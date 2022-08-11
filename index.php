1ccefad5a7f41ab4746d15d0362d32ef014b0025
Joris Delorme <joris.delorme38@gmail.com>
2022-08-11 23:29:16 +0200

    new

--- index.php
+++ index.php
@@ -1,14 +1,17 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Get UDID</title>
-</head>
-<body>
-    <div class="app">
-        <a href="enroll.mobileconfig">Get UDID</a>
-    </div>
-</body>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>UDID</title>
+        <meta name="viewport" content="width=device-width" />
+    </head>
+    <body>
+        <div>
+            <div>
+                <h1>Knowing the UDID of my iOS device</h1>
+                <p>Step 1: Follow the next link and install the certificate</p>  
+                <p> <a class="button" href="get_mobileconfig.php">Give me tap</a> </p>
+                </div>
+            </div>
+    </body>
 </html>
